33a1df90d5c74d89fa5cd58bae813c71e27f962c
Nikita Danila <danila.nikitamihai@gmail.com>
2023-08-11 12:26:57 +0300

    copying files implemented

--- main.py
+++ main.py
@@ -14,61 +14,21 @@ dst_path = Path(args.dst)
 log_path = args.log
 sync_time = args.sync_time
 
-# src_folders, src_files = [], []
-# dst_folders, dst_files = [], []
-
-def write_args_to_file():
-    with open('args_file.txt', 'w') as log_file:
-        log_file.write(src_path)
-        log_file.write("\n")
-        log_file.write(dst_path)
-        log_file.write("\n")
-        log_file.write(log_path)
-        log_file.write("\n")
-        log_file.write(str(sync_time))
-
-
-def sync_files(src_folder, dst_folder):
+def sync_files(src_folder, dst_folder, log_path):
+    #checks if replica folder exists, if not it creates it
     if not Path.exists(dst_folder):
         Path.mkdir(dst_folder)
-
-    for dirpath, dirnames , files in os.walk(src_folder):
-        print(dirpath)
-        relative_path = Path(dirpath) / src_folder
-        print(relative_path)
-        replica_root = Path(dst_folder) / relative_path
-        print(replica_root)
-
+    if not Path('log_file.txt').exists():
+        Path.touch('log_file.txt')
+    for dirpath, dirname, files in os.walk(src_folder):
         for file in files:
-            source_file_path = Path(dirpath)/ file
-            replica_file_path = Path(replica_root)/ file
-            
+            if file not in dst_folder.iterdir():
+                shutil.copy2(Path(dirpath) / file ,dst_folder)
+                print(f"{file} was copied")
+                # Path("log_file.txt").write_text("\n".join(f"{file} was copied"), encoding="utf-8")
+                with open("log_file.txt",'a') as log_file:
+                    log_file.write(f"{file} was copied\n")
 
-def synchronize_folders(source_folder, replica_folder):
-    # Create replica folder if it doesn't exist
-    if not os.path.exists(replica_folder):
-        os.makedirs(replica_folder)
-
-    for root, _, files in os.walk(source_folder):
-        relative_path = os.path.relpath(root, source_folder)
-        replica_root = os.path.join(replica_folder, relative_path)
-
-        for file in files:
-            source_file_path = os.path.join(root, file)
-            replica_file_path = os.path.join(replica_root, file)
-
-            if not os.path.exists(replica_file_path) or \
-               os.path.getmtime(source_file_path) > os.path.getmtime(replica_file_path):
-                # Copy the file from source to replica if it's missing or updated
-                shutil.copy2(source_file_path, replica_file_path)
-                print(f"Copied: {source_file_path} -> {replica_file_path}")
 
 if __name__ == "__main__":
-
-    # sync_files()
-    # walk_src_dir(src_path)
-    # walk_dst_dir(dst_path)
-    # sync_files(src_path, dst_path)
-    synchronize_folders(src_path, dst_path)
-    # print(f'src: {src_folders}, {src_files}')
-    # print(f'dst: {dst_folders}, {dst_files}')
+    sync_files(src_path, dst_path, log_path)
