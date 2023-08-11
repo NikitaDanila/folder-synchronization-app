750d31df39587958939ef832d5fd8da54f4c3252
Nikita Danila <danila.nikitamihai@gmail.com>
2023-08-11 13:20:35 +0300

    copying files and loggin implemented

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-import argparse, shutil, os
+import argparse
+import shutil
+import os
 from pathlib import Path
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-s','--src', help='Path to source folder', required=True)
-parser.add_argument('-d','--dst', help='Path to replica folder',required=True)
-parser.add_argument('-l','--log', help='Path to log file',required=True)
-parser.add_argument('-t','--sync_time', type=int, help='Interval of time for synchronization (sec)', default=10000,required=True)
+parser.add_argument('-s', '--src', help='Path to source folder', required=True)
+parser.add_argument(
+    '-d', '--dst', help='Path to replica folder', required=True)
+parser.add_argument('-l', '--log', help='Path to log file', required=True)
+parser.add_argument('-t', '--sync_time', type=int,
+                    help='Interval of time for synchronization (sec)', default=10000, required=True)
 
 args = parser.parse_args()
 
@@ -14,26 +18,41 @@ dst_path = Path(args.dst)
 log_path = args.log
 sync_time = args.sync_time
 
+
 def sync_files(src_folder, dst_folder, log_path):
-    #checks if replica folder exists, if not it creates it
+    # checks if log file exists, if not it creates it
+    if not Path('log_file.txt').exists():
+        Path(log_path).touch('log_file.txt')
+        print("Log file was created")
+        with open("log_file.txt", 'a') as log_file:
+            log_file.write("Log file was created\n")
+
+    # checks if replica folder exists, if not it creates it
     if not Path.exists(dst_folder):
         Path.mkdir(dst_folder)
-    if not Path('log_file.txt').exists():
-        Path.touch('log_file.txt')
-    for dirpath, dirname, files in os.walk(src_folder):
-        for file in files:
-            if file not in dst_folder.iterdir():
-                shutil.copy2(Path(dirpath) / file ,dst_folder)
-                print(f"{file} was added")
-                # Path("log_file.txt").write_text("\n".join(f"{file} was copied"), encoding="utf-8")
-                with open("log_file.txt",'a') as log_file:
-                    log_file.write(f"{file} was added\n")
-            if Path(file).stat().st_mtime > Path(dst_folder).joinpath(file).stat().st_mtime:
-                print(f"{file} was modified")
-                with open("log_file.txt",'a') as log_file:
-                    shutil.copy2(Path(dirpath) / file ,dst_folder)
-                    log_file.write(f"{file} was modified\n")
+        print("Folder replica was created")
+        with open("log_file.txt", 'a') as log_file:
+            log_file.write("Folder replica was created\n")
 
+    for src_file in src_folder.glob("*"):
+        if src_file.is_file():
+            relative_path = src_file.relative_to(src_folder)
+            dst_file = dst_folder / relative_path
+            match dst_file.exists():
+                case True:
+                    if src_file.stat().st_mtime > dst_file.stat().st_mtime:
+                        shutil.copy2(src_file, dst_file)
+                        print(f"{dst_file} was modified")
+                        with open("log_file.txt", 'a') as log_file:
+                            log_file.write(f"{dst_file} was modified\n")
+                case False:
+                    shutil.copy2(src_file, dst_file)
+                    print(f"{dst_file} was copied")
+                    with open("log_file.txt", 'a') as log_file:
+                        log_file.write(f"{dst_file} was copied\n")
+                    break
+                case _:
+                    pass
 
 
 if __name__ == "__main__":
